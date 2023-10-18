953eb7e3453e3bbe0ec797ff3e104af6017c2b23
Andisssss <61601550+Andisssss@users.noreply.github.com>
2023-10-18 13:24:20 +0300

    small change

--- src/components/AboutMeComponent.vue
+++ src/components/AboutMeComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
       <h1>ABOUT ME</h1>
-      <h3>{{ user.name }}</h3>
-      <h3>{{ user.surname }}</h3>
-      <h3>{{ user.code }}</h3>
+      <h3>Name: {{ user.name }}</h3>
+      <h3>Surname: {{ user.surname }}</h3>
+      <h3>Student code: {{ user.code }}</h3>
     </div>
   </template>
   
